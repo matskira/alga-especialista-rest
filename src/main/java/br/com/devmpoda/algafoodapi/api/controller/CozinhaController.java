c959aede89193b92fa702ea4c0db4f1418a9ecda
Matheus Poda <matheus.poda@gmail.com>
2023-05-23 15:45:56 -0300

    feat(recurso): :sparkles: Criado controller para recurso de listagem de Cozinha
    Criado controller que irá gerenciar as chamadas aos recrusos de cozinha
    e também criado o recurso de listagem de cozinha.

---
+++ src/main/java/br/com/devmpoda/algafoodapi/api/controller/CozinhaController.java
@@ -0,0 +1,24 @@
+package br.com.devmpoda.algafoodapi.api.controller;
+
+import br.com.devmpoda.algafoodapi.domain.model.Cozinha;
+import br.com.devmpoda.algafoodapi.domain.repository.CozinhaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cozinhas")
+public class CozinhaController {
+    @Autowired
+    private CozinhaRepository cozinhaRepository;
+
+    @GetMapping
+    public List<Cozinha> listar() {
+        return cozinhaRepository.todas();
+    }
+
+}
