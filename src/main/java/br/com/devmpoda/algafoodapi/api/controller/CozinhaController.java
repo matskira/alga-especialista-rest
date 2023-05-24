d4696f46b2b9ff59854ece7c2170414567269ee2
Matheus Poda <matheus.poda@gmail.com>
2023-05-24 10:26:36 -0300

    feat(recurso): :sparkles: Criado recurso novo Cozinha
    Recurso responsável por buscar cozinha por um ID.

--- src/main/java/br/com/devmpoda/algafoodapi/api/controller/CozinhaController.java
+++ src/main/java/br/com/devmpoda/algafoodapi/api/controller/CozinhaController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.websocket.server.PathParam;
 import java.util.List;
 
 @RestController
@@ -17,8 +19,14 @@ public class CozinhaController {
     @Autowired
     private CozinhaRepository cozinhaRepository;
 
+    @GetMapping
     public List<Cozinha> listar() {
         return cozinhaRepository.todas();
+    }
+
+    @GetMapping("/{id}")
+    public Cozinha buscar(@PathVariable Long id) {
+        return cozinhaRepository.porId(id);
     }
 
 }
