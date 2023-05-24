c3d3fbddb01f1c41e6660020a741029a74c91cbc
Matheus Poda <matheus.poda@gmail.com>
2023-05-24 10:00:53 -0300

    feat(recurso): Adicionado padrão de formato de saída como JSON

--- src/main/java/br/com/devmpoda/algafoodapi/api/controller/CozinhaController.java
+++ src/main/java/br/com/devmpoda/algafoodapi/api/controller/CozinhaController.java
@@ -3,6 +3,7 @@ package br.com.devmpoda.algafoodapi.api.controller;
 import br.com.devmpoda.algafoodapi.domain.model.Cozinha;
 import br.com.devmpoda.algafoodapi.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,12 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/cozinhas")
+@RequestMapping(value = "/cozinhas", produces = MediaType.APPLICATION_JSON_VALUE)
 public class CozinhaController {
     @Autowired
     private CozinhaRepository cozinhaRepository;
 
-    @GetMapping
     public List<Cozinha> listar() {
         return cozinhaRepository.todas();
     }
