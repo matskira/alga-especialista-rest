69df8db0513859dbe1dd013759614529763e9ab6
Matheus Poda <matheus.poda@gmail.com>
2023-06-14 10:36:39 -0300

    :sparkles: feat(exception): Exceptions de dominio
    Criação de duas excpetion que irão nos auxiliar para caso não encontre
    um determinado item na base de dados e não for possível de realizar
    qualquer tipo de transação por já uso da entidade

---
+++ src/main/java/br/com/devmpoda/algafoodapi/domain/exception/EntidadeEmUsoException.java
@@ -0,0 +1,11 @@
+package br.com.devmpoda.algafoodapi.domain.exception;
+
+public class EntidadeEmUsoException extends RuntimeException{
+
+    private static final long serialVersionUID = 1L;
+
+    public EntidadeEmUsoException(String mensagem) {
+        super(mensagem);
+    }
+
+}
