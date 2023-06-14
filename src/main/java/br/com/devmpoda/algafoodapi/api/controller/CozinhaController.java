882050a3870151cf28f484a8fff3d4b977886fff
Matheus Poda <matheus.poda@gmail.com>
2023-06-14 11:10:49 -0300

    fix(code): Removido XML

--- src/main/java/br/com/devmpoda/algafoodapi/api/controller/CozinhaController.java
+++ src/main/java/br/com/devmpoda/algafoodapi/api/controller/CozinhaController.java
@@ -1,6 +1,5 @@
 package br.com.devmpoda.algafoodapi.api.controller;
 
-import br.com.devmpoda.algafoodapi.api.model.CozinhasXmlWrapper;
 import br.com.devmpoda.algafoodapi.domain.exception.EntidadeEmUsoException;
 import br.com.devmpoda.algafoodapi.domain.exception.EntidadeNaoEncontradaException;
 import br.com.devmpoda.algafoodapi.domain.model.Cozinha;
@@ -8,14 +7,11 @@ import br.com.devmpoda.algafoodapi.domain.repository.CozinhaRepository;
 import br.com.devmpoda.algafoodapi.domain.service.CadastroCozinhaService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import javax.websocket.server.PathParam;
 import java.util.List;
 
 @RestController
@@ -31,12 +27,6 @@ public class CozinhaController {
     public List<Cozinha> listar() {
         return cozinhaRepository.todas();
     }
-
-    @GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
-    public CozinhasXmlWrapper listarXml() {
-        return new CozinhasXmlWrapper(cozinhaRepository.todas());
-    }
-
 
     @GetMapping("/{id}")
     public ResponseEntity<Cozinha> buscar(@PathVariable Long id) {
