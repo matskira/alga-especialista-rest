339624a9e665a7b94c3bb02ad770f09015cdf1e8
Matheus Poda <matheus.poda@gmail.com>
2023-06-16 09:38:39 -0300

    :sparkles: feat(code): Recurso para CRUD do recurso Estado

---
+++ src/main/java/br/com/devmpoda/algafoodapi/domain/service/CadastroEstadoService.java
@@ -0,0 +1,35 @@
+package br.com.devmpoda.algafoodapi.domain.service;
+
+import br.com.devmpoda.algafoodapi.domain.exception.EntidadeEmUsoException;
+import br.com.devmpoda.algafoodapi.domain.exception.EntidadeNaoEncontradaException;
+import br.com.devmpoda.algafoodapi.domain.model.Cozinha;
+import br.com.devmpoda.algafoodapi.domain.model.Estado;
+import br.com.devmpoda.algafoodapi.domain.repository.CozinhaRepository;
+import br.com.devmpoda.algafoodapi.domain.repository.EstadoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CadastroEstadoService {
+
+    @Autowired
+    EstadoRepository estadoRepository;
+
+    public Estado salvar(Estado estado) {
+        return estadoRepository.adicionar(estado);
+    }
+
+    public void excluir(Long id) {
+        try {
+            estadoRepository.remover(id);
+        }catch (EmptyResultDataAccessException ex){
+            throw new EntidadeNaoEncontradaException(
+                    String.format("Não existe um cadastro de Estado com código %d", id));
+        }catch(DataIntegrityViolationException ex){
+            throw new EntidadeEmUsoException(
+                    String.format("Estado de código %d não pode ser removida, pois está em uso", id));
+        }
+    }
+}
