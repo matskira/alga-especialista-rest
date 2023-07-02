f8a39b45657c31a357f4c2d25ab51289d88a6f29
Matheus Poda <matheus.poda@gmail.com>
2023-07-02 15:24:37 -0300

    :sparkles: feat(code): Criado Entidade Grupo e seu relacionamento com a Entidade Permissao

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -37,5 +37,12 @@ insert into forma_pagamento (id, descricao) values (3, 'Dinheiro');
 insert into permissao (id, nome, descricao) values (1, 'CONSULTAR_COZINHAS', 'Permite consultar cozinhas');
 insert into permissao (id, nome, descricao) values (2, 'EDITAR_COZINHAS', 'Permite editar cozinhas');
 
+insert into grupo (id, nome) values (1, 'Gerente');
+insert into grupo (id, nome) values (2, 'Atendente');
+insert into grupo (id, nome) values (3, 'Caixa');
+
+insert into grupo_permissao (grupo_id, permissao_id) values (1, 1);
+insert into grupo_permissao (grupo_id, permissao_id) values (1, 2);
+
 insert into restaurante_forma_pagamento (restaurante_id, forma_pagamento_id) values (1, 1), (1, 2), (1, 3), (2, 3), (3, 2), (3, 3);
 insert into restaurante_forma_pagamento (restaurante_id, forma_pagamento_id) values (1, 1), (1, 2), (1, 3), (2, 3), (3, 2), (3, 3), (4, 1), (4, 2), (5, 1), (5, 2), (6, 3);
