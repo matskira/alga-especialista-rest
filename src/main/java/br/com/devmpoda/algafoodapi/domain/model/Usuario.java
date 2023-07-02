bbc6658b5f7b7cdfb4982c5a76944aae2b7b134b
Matheus Poda <matheus.poda@gmail.com>
2023-07-02 15:42:52 -0300

    :sparkles: feat(code): Criado Entidade Usuario e seu relacionamento com a Entidade Grupo

---
+++ src/main/java/br/com/devmpoda/algafoodapi/domain/model/Usuario.java
@@ -0,0 +1,42 @@
+package br.com.devmpoda.algafoodapi.domain.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String senha;
+
+    @CreationTimestamp
+    @Column(nullable = false, columnDefinition = "datetime")
+    private LocalDateTime dataCadastro;
+
+    @ManyToMany
+    @JoinTable(name="usuario_grupo",
+            joinColumns = @JoinColumn(name = "usuario_id"),
+            inverseJoinColumns = @JoinColumn(name = "grupo_id"))
+    private List<Grupo> grupos = new ArrayList<>();
+
+}
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -44,5 +44,11 @@ insert into grupo (id, nome) values (3, 'Caixa');
 insert into grupo_permissao (grupo_id, permissao_id) values (1, 1);
 insert into grupo_permissao (grupo_id, permissao_id) values (1, 2);
 
+insert into usuario (id, nome, email, senha, data_cadastro) values (1, 'João Silva', 'teste@teste.com.br', '$2a$1', utc_timestamp);
+insert into usuario (id, nome, email, senha, data_cadastro) values (2, 'Maria Souza', 'teste2@teste.com.br', '$2a$2', utc_timestamp);
+
+insert into usuario_grupo (usuario_id, grupo_id) values (1, 1);
+insert into usuario_grupo (usuario_id, grupo_id) values (2, 2);
+
 insert into restaurante_forma_pagamento (restaurante_id, forma_pagamento_id) values (1, 1), (1, 2), (1, 3), (2, 3), (3, 2), (3, 3);
 insert into restaurante_forma_pagamento (restaurante_id, forma_pagamento_id) values (1, 1), (1, 2), (1, 3), (2, 3), (3, 2), (3, 3), (4, 1), (4, 2), (5, 1), (5, 2), (6, 3);
