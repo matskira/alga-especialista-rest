d6257684421d42b3058cfa4b62b4a1ede058356f
Matheus Poda <matheus.poda@gmail.com>
2023-07-25 20:36:50 -0300

    Refatorado para a RFC7807

--- src/main/java/br/com/devmpoda/algafoodapi/api/controller/CidadeController.java
+++ src/main/java/br/com/devmpoda/algafoodapi/api/controller/CidadeController.java
@@ -1,27 +1,16 @@
 package br.com.devmpoda.algafoodapi.api.controller;
 
-import br.com.devmpoda.algafoodapi.api.exceptionhandler.Problema;
-import br.com.devmpoda.algafoodapi.domain.exception.EntidadeEmUsoException;
-import br.com.devmpoda.algafoodapi.domain.exception.EntidadeNaoEncontradaException;
 import br.com.devmpoda.algafoodapi.domain.exception.EstadoNaoEncontradaException;
 import br.com.devmpoda.algafoodapi.domain.exception.NegocioException;
 import br.com.devmpoda.algafoodapi.domain.model.Cidade;
-import br.com.devmpoda.algafoodapi.domain.model.Cozinha;
-import br.com.devmpoda.algafoodapi.domain.model.Estado;
-import br.com.devmpoda.algafoodapi.domain.model.Restaurante;
 import br.com.devmpoda.algafoodapi.domain.repository.CidadeRepository;
 import br.com.devmpoda.algafoodapi.domain.service.CadastroCidadeService;
-import br.com.devmpoda.algafoodapi.domain.service.CadastroEstadoService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/cidades")
--- src/main/java/br/com/devmpoda/algafoodapi/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/br/com/devmpoda/algafoodapi/api/exceptionhandler/ApiExceptionHandler.java
@@ -6,7 +6,6 @@ import br.com.devmpoda.algafoodapi.domain.exception.NegocioException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -19,36 +18,59 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(EntidadeNaoEncontradaException.class)
     public ResponseEntity<?> handlerEntidadeNaoEncontradaException(EntidadeNaoEncontradaException e, WebRequest request) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        String detail = e.getMessage();
 
-        return this.handleExceptionInternal(e, e.getMessage(), new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+        ProblemType problemType = ProblemType.ENTIDADE_NAO_ENCONTRADO;
+        Problem problem = createProblemBuilder(status, problemType, detail).build();
+
+        return this.handleExceptionInternal(e, problem, new HttpHeaders(), status, request);
     }
 
     @ExceptionHandler(EntidadeEmUsoException.class)
     public ResponseEntity<?> handlerEntidadeEmUsoException(EntidadeEmUsoException e,  WebRequest request) {
 
-        return this.handleExceptionInternal(e, e.getMessage(), new HttpHeaders(), HttpStatus.CONFLICT, request);
+        HttpStatus status = HttpStatus.CONFLICT;
+        String detail = e.getMessage();
+
+        ProblemType problemType = ProblemType.ENTIDADE_NAO_ENCONTRADO;
+        Problem problem = createProblemBuilder(status, problemType, detail).build();
+
+        return this.handleExceptionInternal(e, problem, new HttpHeaders(), status, request);
     }
 
     @ExceptionHandler(NegocioException.class)
     public ResponseEntity<?> handlerNegocioException(NegocioException e,  WebRequest request) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        String detail = e.getMessage();
 
-        return this.handleExceptionInternal(e, e.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+        ProblemType problemType = ProblemType.ERRO_NEGOCIO;
+        Problem problem = createProblemBuilder(status, problemType, detail).build();
+        return this.handleExceptionInternal(e, problem, new HttpHeaders(), status, request);
     }
 
     @Override
     protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
         if (body == null){
-            body = Problema.builder()
-                    .dataHora(LocalDateTime.now())
-                    .mensagem(status.getReasonPhrase())
+            body = Problem.builder()
+                    .title(status.getReasonPhrase())
+                    .status(status.value())
                     .build();
         } else if (body instanceof String){
-            body = Problema.builder()
-                    .dataHora(LocalDateTime.now())
-                    .mensagem((String) body)
+            body = Problem.builder()
+                    .title((String) body)
+                    .status(status.value())
                     .build();
         }
 
         return super.handleExceptionInternal(ex, body, headers, status, request);
+    }
+
+    private Problem.ProblemBuilder createProblemBuilder(HttpStatus status, ProblemType type, String detail) {
+        return Problem.builder()
+                .status(status.value())
+                .type(type.getUri())
+                .title(type.getTitle())
+                .detail(detail);
     }
 }
