d33db9880425d0a300b62e917cfab2d370b8d20e
Matheus Poda <matheus.poda@gmail.com>
2023-08-01 18:46:59 -0300

    feat(code): Tratamento de exception para casos de InvalidFormatException

--- src/main/java/br/com/devmpoda/algafoodapi/api/exceptionhandler/ProblemType.java
+++ src/main/java/br/com/devmpoda/algafoodapi/api/exceptionhandler/ProblemType.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 @Getter
 public enum ProblemType {
 
+    MENSAGEM_INCOMPREENSIVEL("/mensagem-incompreensivel", "Mensagem incompreensível"),
     ENTIDADE_NAO_ENCONTRADO("/entidade-nao-encontrada", "Entidade não encontrada"),
     ENTIDADE_EM_USO("/entidade-em-uso", "Entidade em uso"),
     ERRO_NEGOCIO("/erro-negocio", "Violação de regra de negócio");
