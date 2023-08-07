76b63a4b344f069e6bff1b2f89b706fd2e4c2779
Matheus Poda <matheus.poda@gmail.com>
2023-08-07 16:11:24 -0300

    Adicionado tratamento de exceção em caso de path param diferente do esperado

--- src/main/java/br/com/devmpoda/algafoodapi/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/br/com/devmpoda/algafoodapi/api/exceptionhandler/ApiExceptionHandler.java
@@ -9,17 +9,17 @@ import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import com.fasterxml.jackson.databind.exc.PropertyBindingException;
 import com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException;
 import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.springframework.beans.TypeMismatchException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.InvalidMediaTypeException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -45,6 +45,27 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
         Problem problem = createProblemBuilder(status, problemType, detail).build();
 
         return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
+    }
+
+
+    @Override
+    protected ResponseEntity<Object> handleTypeMismatch(TypeMismatchException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        if (ex instanceof MethodArgumentTypeMismatchException){
+            return handleMethodArgumentTypeMismatchException((MethodArgumentTypeMismatchException)ex,headers, status, request);
+        }
+
+        return super.handleTypeMismatch(ex, headers, status, request);
+    }
+
+    private ResponseEntity<Object> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException ex,HttpHeaders headers, HttpStatus status, WebRequest request) {
+        ProblemType problemType = ProblemType.PROPRIEDADES_INVALIDAS;
+
+        String detail = String.format("O parâmetro de URL '%s' recebeu o valor '%s', que é um tipo inválido. " +
+                "Corrija e informe um valor compatível com o tipo %s", ex.getName(), ex.getValue(), ex.getRequiredType().getSimpleName());
+        Problem problem = createProblemBuilder(status, problemType, detail).build();
+
+        return handleExceptionInternal(ex, problem, headers, status, request);
     }
 
     private ResponseEntity<Object> handlePropertyBindingException(PropertyBindingException rootCause, HttpHeaders headers, HttpStatus status, WebRequest request) {
--- src/main/java/br/com/devmpoda/algafoodapi/api/exceptionhandler/ProblemType.java
+++ src/main/java/br/com/devmpoda/algafoodapi/api/exceptionhandler/ProblemType.java
@@ -9,6 +9,7 @@ public enum ProblemType {
     MENSAGEM_INCOMPREENSIVEL("/mensagem-incompreensivel", "Mensagem incompreensível"),
     ENTIDADE_NAO_ENCONTRADO("/entidade-nao-encontrada", "Entidade não encontrada"),
     ENTIDADE_EM_USO("/entidade-em-uso", "Entidade em uso"),
+    PARAMETRO_INVALIDO("/parametro-invalido", "Parâmetro inválido"),
     ERRO_NEGOCIO("/erro-negocio", "Violação de regra de negócio");
 
     private String title;
